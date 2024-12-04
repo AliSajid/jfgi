e233a483e37053077c84e8f3faade1f31547ea88
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-12-04 12:49:43 -0500

    build: removed the basicssl plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vitest/config';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  plugins: [
-    enhancedImages(),
-    sveltekit(),
-    basicSsl({
-      name: 'localhost'
-    })
-  ],
+  plugins: [enhancedImages(), sveltekit()],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
   }
